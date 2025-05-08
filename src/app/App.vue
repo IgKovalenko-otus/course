78780468e833ff2afbca262ffa073debb8319f01
ig.kovalenko <ig.kovalenko-otus@outlook.com>
2025-05-08 21:21:55 +0300

    добавление sass/scss, fix файловой структуры

---
+++ src/app/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import HomePage from "../components/route/home/HomePage.vue";
+</script>
+
+<template>
+  <div class="app">
+    <HomePage/>
+  </div>
+</template>
+
+<style src="./styles/app.scss" lang="scss"></style>
+
---
+++ src/components/route/home/HomePage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import Page from "../../blocks/Page/Page.vue";
+</script>
+
+<template>
+  <Page>
+    <img src="/svg/vite.svg" alt="Vite logo" />
+    <img src="/svg/vue.svg" alt="Vite logo" />
+  </Page>
+</template>
