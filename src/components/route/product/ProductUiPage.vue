9ca7e47d43c9a8ed3dffa1e69bc9b28f2aa94321
ig.kovalenko <ig.kovalenko-otus@outlook.com>
2025-05-16 20:53:03 +0300

    добавление странички продукта, данных всех продуктов

---
+++ src/components/route/product/ProductUiPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    import UiPageContent from 'blocks/Page/Content/UiPageContent.vue';
+    import UiPage from 'blocks/Page/UiPage.vue';
+</script>
+
+<template>
+    <UiPage>
+        <UiPageContent>
+            Страничка продукта
+        </UiPageContent>
+    </UiPage>
+</template>
