940d1716d8556a9ca9db822748b507f256ed7580
ig.kovalenko <ig.kovalenko-otus@outlook.com>
2025-06-07 23:13:35 +0300

    доработка ленивая загрузка для страничек кроме домашней

--- src/components/app/logic/router.ts
+++ src/components/app/logic/router.ts
@@ -1,9 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
 
 import HomeUiPage from 'route/home/HomeUiPage.vue';
-import BasketUiPage from 'route/basket/BasketUiPage.vue';
-import AdminUiPage from 'route/admin/AdminUiPage.vue';
-import ProductUiPage from 'route/product/ProductUiPage.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,16 +13,16 @@ const router = createRouter({
         {
             path: '/basket',
             name: 'basket',
-            component: BasketUiPage
+            component: () => import('route/basket/BasketUiPage.vue')
         },
         {
             path: '/admin',
             name: 'admin',
-            component: AdminUiPage
+            component: () => import('route/admin/AdminUiPage.vue')
         },
         {
             path: '/product/:id',
-            component: ProductUiPage
+            component: () => import('route/product/ProductUiPage.vue')
         },
     ],
 })
